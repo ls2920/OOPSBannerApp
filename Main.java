10c10209a5b3e9e4dc6686b64330138581544480
ls2920 <ls2920@srmist.edu.in>
2026-02-22 12:04:50 +0530

    UC3

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(String.join("  ******  " , "  ******  " , " ********  " , "  ******  "));
-        System.out.println(String.join(" **    ** " , " **    ** " , " **    **  " , " **    ** "));
-        System.out.println(String.join("**      **" , "**      **" , " **    **  " , " **       "));
-        System.out.println(String.join("**      **" , "**      **" , " ********  " , "  *****   "));
-        System.out.println(String.join("**      **" , "**      **" , " **        " , "       ** "));
-        System.out.println(String.join(" **    ** " , " **    ** " , " **        " , " **    ** "));
-        System.out.println(String.join("  ******  " , "  ******  " , " **        " , "  *****   "));
+        System.out.println(String.join(" ","  ******  " , "  ******  " , " ********  " , "  ******  "));
+        System.out.println(String.join(" "," **    ** " , " **    ** " , " **    **  " , " **    ** "));
+        System.out.println(String.join(" ","**      **" , "**      **" , " **    **  " , " **       "));
+        System.out.println(String.join(" ","**      **" , "**      **" , " ********  " , "  *****   "));
+        System.out.println(String.join(" ","**      **" , "**      **" , " **        " , "       ** "));
+        System.out.println(String.join(" "," **    ** " , " **    ** " , " **        " , " **    ** "));
+        System.out.println(String.join(" ","  ******  " , "  ******  " , " **        " , "  *****   "));
     }
 }
