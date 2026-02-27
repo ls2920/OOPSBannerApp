49ca42dea273a15b8e65411d75610182085617ca
ls2920 <ls2920@srmist.edu.in>
2026-02-27 18:24:29 +0530

    feature/UC6

--- Main.java
+++ Main.java
@@ -1,19 +1,51 @@
 public class Main
 {
-	public static void main(String args[])
-	{
-		String lines[]={
-			String.join(" ","  ******  " ,"  ******  " ," ********  " ,"  ******  " ),
-			String.join(" "," **    ** " ," **    ** " ," **    **  " ," **    ** " ),
-			String.join(" ","**      **" ,"**      **" ," **    **  " ," **       " ),
-			String.join(" ","**      **" ,"**      **" ," ********  " ,"  *****   " ),
-			String.join(" ","**      **" ,"**      **" ," **        " ,"       ** " ),
-			String.join(" "," **    ** " ," **    ** " ," **        " ," **    ** " ),
-			String.join(" ","  ******  " ,"  ******  " ," **        " ,"  *****   " )
-			};
-		for (int i=0; i<(lines.length); i++)
+	public static String[] getOPattern(){
+		return new String[] 
 		{
-			System.out.println(lines[i]);
+			 "  ******  " ,
+			 " **    ** " ,
+			 "**      **" ,
+			 "**      **" ,
+			 "**      **" ,
+			 " **    ** " ,
+			 "  ******  " 
+		};
+	}	
+	public static String[] getpPattern(){
+		return new String[] 
+		{
+			 " ********  " ,
+			 " **    **  " ,
+			 " **    **  " ,
+			 " ********  " ,
+			 " **        " ,
+			 " **        " ,
+			 " **        " 
+		};
+	}	
+	public static String[] getsPattern(){
+		return new String[] 
+		{
+			 "  ******  " ,
+			 " **    ** " ,
+			 " **       " ,
+			 "  *****   " ,
+			 "       ** " ,
+			 " **    ** " ,
+			 "  *****   "
+		};
+	}	
+	
+	public static void main(String[] args)
+	{
+		String[] oPattern = getOPattern();
+		String[] pPattern = getpPattern();
+		String[] sPattern = getsPattern();
+		
+		for (int i=0; i< oPattern.length; i++)
+		{
+			System.out.println(String.join(" ",oPattern[i],pPattern[i],sPattern[i]));
 		}
 	}
 }
