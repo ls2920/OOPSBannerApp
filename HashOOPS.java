7e39d2d77c71991a9b86f39741ba4591a8ce91f9
ls2920 <ls2920@srmist.edu.in>
2026-03-09 23:07:49 +0530

    UC8

---
+++ HashOOPS.java
@@ -0,0 +1,61 @@
+import java.util.HashMap;
+
+public class HashOOPS
+{
+    public static HashMap<Character, String[]> createCharacterMap()
+    {
+        HashMap<Character, String[]> charMap = new HashMap<>();
+
+        charMap.put('O', new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        });
+
+        charMap.put('P', new String[]{
+            "****** ",
+            "*     *",
+            "****** ",
+            "*      ",
+            "*      "
+        });
+
+        charMap.put('S', new String[]{
+            " ***** ",
+            "*      ",
+            " ***** ",
+            "      *",
+            " ***** "
+        });
+
+        return charMap;
+    }
+
+    public static void displayBanner(String message, HashMap<Character, String[]> charMap)
+    {
+        int patternHeight = charMap.get('O').length;
+
+        for(int line = 0; line < patternHeight; line++)
+        {
+            for(int i = 0; i < message.length(); i++)
+            {
+                char ch = message.charAt(i);
+
+                if(charMap.containsKey(ch))
+                    System.out.print(charMap.get(ch)[line] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[])
+    {
+        HashMap<Character, String[]> charMap = createCharacterMap();
+
+        String message = "OOPS";
+
+        displayBanner(message, charMap);
+    }
+}
